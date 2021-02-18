cc7ead9de67ea9ea80c8bf5325fdac3230c43cee
starhwong <1154022874@qq.com>
2021-02-18 11:26:14 +0800

    暂存

---
+++ echarts/demo/demo/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 将自动注册所有组件为全局组件
+import dataV from '@jiaminghi/data-view'
+
+Vue.use(dataV)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ echarts/demo/demo/src/views/databroad/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <dv-full-screen-container class="bg">
+      <div class="head">
+        <div class="empty" />
+        <div class="center">
+          <span class="title">车联网智能监控大屏</span>
+        </div>
+        <div class="datetime">
+          <span>2021-02-18 10:56:11 星期四</span>
+        </div>
+      </div>
+      <div class="body-box">
+
+      </div>
+    </dv-full-screen-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataView'
+}
+</script>
+
+<style lang="scss" scoped>
+.bg {
+  padding: 0.2rem 0.2rem 0 0.2rem;
+  background-image: url("../../assets/pageBg.png");
+  background-position: center center;
+  background-size: cover;
+}
+.head {
+  display: flex;
+  height: 2em;
+  line-height: 2em;
+  font-size: 18px;
+  font-weight: bolder;
+  color: white;
+  .empty {
+    width: 28%;
+    background-color: red;
+  }
+  .center {
+    flex: 1;
+    text-align: center;
+    background-color: green;
+  }
+  .datetime {
+    width: 28%;
+    text-align: center;
+    // background-color: yellow;
+  }
+}
+</style>
